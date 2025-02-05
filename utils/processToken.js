4ee5250feadf80b13a6db863256fada8db418586
byf1sh <140485957+byf1sh@users.noreply.github.com>
2025-02-06 00:11:19 +0700

    Update processToken.js

--- utils/processToken.js
+++ utils/processToken.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const jwt_secret = process.env.JWT_SECRET;
+const jwt_secret = "ABCDEFGHIJKLMNOPQRSUTAWawdilh!@#!@";
 module.exports = {
   generateToken: (data) => {
     const token = jwt.sign(data, jwt_secret);
